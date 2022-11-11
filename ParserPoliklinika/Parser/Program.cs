032b640a52e6cb982f7ad6a57abca50581b7def8
Fa1L61 <81235390+Fa1L61@users.noreply.github.com>
2022-11-11 11:27:42 +0300

    Add files via upload

---
+++ ParserPoliklinika/Parser/Program.cs
@@ -0,0 +1,35 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections;
+
+namespace Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webGet = new HtmlWeb();
+            HtmlDocument document = webGet.Load("https://prodoctorov.ru/rostov-na-donu/top/poliklinika/");
+            
+            ArrayList pol = new ArrayList();
+            ArrayList address = new ArrayList();
+
+            document = webGet.Load("https://prodoctorov.ru/rostov-na-donu/top/poliklinika/?page=0" );
+            foreach (HtmlNode node in document.DocumentNode.SelectNodes("//div[@class='b-card__name']//a[@class='b-card__name-link b-link ui-text ui-text_h5 ui-text_color_primary-blue']"))
+            {
+                pol.Add(node.InnerText.Replace("\n", "").Trim());
+            }
+            foreach (HtmlNode node in document.DocumentNode.SelectNodes("//div[@class='b-card__address']"))
+            {
+                address.Add(node.InnerText.Replace("\n", "").Trim());
+            }
+            
+
+            for (int i = 0; i < address.Count; i++)
+            {
+                Console.WriteLine(pol[i].ToString() + " расположена по адресу: " + address[i].ToString());
+            }
+
+        }
+    }
+}
